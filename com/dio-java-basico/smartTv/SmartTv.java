e848d049285fe4eb9cd33c0c9d61875f9217654b
Bruno Carvalho <brunocarvalho.devsenai@gmail.com>
2024-07-14 05:55:58 -0300

    inserção de documentação no codigo

--- com/dio-java-basico/smartTv/SmartTv.java
+++ com/dio-java-basico/smartTv/SmartTv.java
@@ -1,9 +1,32 @@
+/**
+ * <h1>Smart Tv</h1>
+ * <b>Classe desenvolvida para simular as funçoes de uma TV</b>
+ * <p>
+ * como a função de <b>Ligar</b> e <b>Desligar</b>
+ * <p>
+ * as funçoes de <b>Inserir o numero do canal desejado</b>
+ * <p>
+ * <b>Aumentar</b> e <b>Diminur</b> o <b>Volume o canal</b>
+ * 
+ * @author Bruno Carvalho
+ * @version 1.0
+ * @since 14/07/2024
+ */
 public class SmartTv {
     private boolean tvOnOff = false;
     private int canalTv = 1;
     private int volumeTv = 25;
 
-    //ligar e desligar tv
+    /**
+     * Este método é utilizado para ligar e desligar tv
+     * <p>
+     * Achamada do método consiste em <b>Ligar</b> a Tv caso esteja
+     * <b>desligada</b>,
+     * E <b>Desligar</b> caso esteja <b>ligada</b>;
+     * 
+     * @return tvOnOff Retornado <b>true</b> para ligada e <b>false</b> para
+     *         desligada
+     **/
     public boolean LigarTv() {
         boolean onOff;
         onOff = (tvOnOff == false) ? true : false;
@@ -12,6 +35,20 @@ public class SmartTv {
         return tvOnOff;
     }
 
+    /**
+     * Este método é utilizado para acressentar 1 (hum) nível o volume da Tv;
+     * <p>
+     * O método faz a leitura do valor da vairavél <b>volumeTv</b>, e acressenta
+     * mais <b>1</b> (um)
+     * a o seu valor, <b>desde que</b> o valor da variavél <b>volumeTv</b> seja <b><
+     * (menor)</b> que <b>50 (cinquenta)</b>, retorna o novo valor atualizado da
+     * variavél <b>volumeTv</b>;
+     * 
+     * @param volumeTv Volume atual da tv;
+     * 
+     * @return Retorna o valor atualizado da variavél <b>volumeTv</b>
+     *
+     */
     public int TvVolumeUp() {
 
         if ((tvOnOff == true) && (volumeTv < 50)) {
@@ -22,6 +59,23 @@ public class SmartTv {
         return volumeTv;
     }
 
+    /**
+     * Este método é utilizado para subtrair 1 (hum) nível ao volume da Tv;
+     * <p>
+     * O método faz a leitura do valor da vairavél <b>volumeTv</b>, e subtrair
+     * <b>1 (um)</b> ao seu valor, <b>desde que</b> o valor da variavél
+     * <b>canalTv</b> seja <b> >=
+     * (maior ou igual)</b> a <b>1 (um)</b>, <b>e</b> o valor da variavel tvOnOff
+     * seja
+     * <b>true</b>, retorna o novo valor atualizado da variavél <b>volumeTv</b>;
+     * 
+     * @param volumeTv Volume atual da tv;
+     *
+     * @param tvOnOff  Valor da TV <b>Ligada</b> ou <b>Desligada</b>
+     * 
+     * @return Retorna o valor atualizado da variavél <b>volumeTv</b>
+     *
+     */
     public int TvVolumeDown() {
 
         if ((tvOnOff == true) && (volumeTv > 1)) {
@@ -42,6 +96,22 @@ public class SmartTv {
         return canalTv;
     }
 
+    /**
+     * Este método é utilizado para acressentar 1 (hum) nível ao canal da Tv;
+     * <p>
+     * O método faz a leitura do valor da vairavél <b>canalTv</b>, e acressenta
+     * mais <b>1 (um)</b> ao seu valor, <b>desde que</b> o valor da variavél
+     * <b>canalTv</b> seja <b><
+     * (menor)</b> que <b>100 (cem)</b>, <b>e</b> o valor da variavel tvOnOff seja
+     * <b>true</b>, retorna o novo valor atualizado da variavél <b>canalTv</b>;
+     * 
+     * @param canalTv Volume atual da tv;
+     *
+     * @param tvOnOff Valor da TV <b>Ligada</b> ou <b>Desligada</b>
+     * 
+     * @return Retorna o valor atualizado da variavél <b>canalTv</b>
+     *
+     */
     public int CanalUp() {
 
         if ((tvOnOff == true) && (canalTv < 100)) {
@@ -52,6 +122,22 @@ public class SmartTv {
         return canalTv;
     }
 
+    /**
+     * Este método é utilizado para subtrair 1 (hum) nível ao canal da Tv;
+     * <p>
+     * O método faz a leitura do valor da vairavél <b>canalTv</b>, e subtrair
+     * mais <b>1 (um)</b> ao seu valor, <b>desde que</b> o valor da variavél
+     * <b>canalTv</b> seja <b>>
+     * (maior)</b> que <b>1 (um)</b>, <b>e</b> o valor da variavel tvOnOff seja
+     * <b>true</b>, retorna o novo valor atualizado da variavél <b>canalTv</b>;
+     * 
+     * @param canalTv Volume atual da tv;
+     *
+     * @param tvOnOff Valor da TV <b>Ligada</b> ou <b>Desligada</b>
+     * 
+     * @return Retorna o valor atualizado da variavél <b>canalTv</b>
+     *
+     */
     public int CanalDown() {
 
         if ((tvOnOff == true) && (canalTv > 1)) {
@@ -62,9 +148,10 @@ public class SmartTv {
         return canalTv;
     }
 
-    public String StatusTv (){
+    public String StatusTv() {
 
-        String status = ("\nA tv está ligada? - " + tvOnOff + "\nA tv no canal ? - " + canalTv  +"\nA tv está no volume? - " + volumeTv);
+        String status = ("\nA tv está ligada? - " + tvOnOff + "\nA tv no canal ? - " + canalTv
+                + "\nA tv está no volume? - " + volumeTv);
 
         return status;
     }

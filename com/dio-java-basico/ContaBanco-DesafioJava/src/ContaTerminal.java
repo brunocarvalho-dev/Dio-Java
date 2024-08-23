2ddfc68eb6bf818dc01ecfeae87b64138afc668a
Bruno Carvalho <brunocarvalho.devsenai@gmail.com>
2024-08-23 02:37:21 -0300

    Conmclusão desafio de projeto Dio java Basico, conta terminal

--- com/dio-java-basico/ContaBanco-DesafioJava/src/ContaTerminal.java
+++ com/dio-java-basico/ContaBanco-DesafioJava/src/ContaTerminal.java
@@ -1,2 +1,42 @@
-package PACKAGE_NAME;public class ContaTerminal {
+import java.util.Scanner;
+
+public class ContaTerminal {
+    private int numero;
+    private String agencia;
+    private String nomeCliente;
+    private double saldo;
+
+    public ContaTerminal(int numero, String agencia, String nomeCliente, double saldo) {
+        this.numero = numero;
+        this.agencia = agencia;
+        this.nomeCliente = nomeCliente;
+        this.saldo = saldo;
+    }
+
+    public void boasVindas() {
+        String ola = String.format("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s ",nomeCliente, agencia);
+        String contaDador = String.format("conta " + numero + " ");
+        String valor = String.format("e seu saldo %.2f já está disponível para saque!",saldo);
+        System.out.println(ola.concat(contaDador.concat(valor)));
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Por favor, digite seu nome: ");
+        String nome = sc.nextLine();
+
+        System.out.println(String.format("Por favor %s, digite sua agência: ",nome));
+        String agencia = sc.nextLine();
+
+        System.out.println(String.format("Por favor %s, digite sua conta: ",nome));
+        int contaNumero = sc.nextInt();
+
+        System.out.println(String.format("Por favor %s, digite seu saldo: ",nome));
+        Double valor = sc.nextDouble();
+
+        ContaTerminal contaTerminal = new ContaTerminal(contaNumero, agencia, nome, valor);
+
+        contaTerminal.boasVindas();
+    }
 }
